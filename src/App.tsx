feddc9ad94801a9d2d389dbb4caf80899f942f62
Hostlife22 <sima.serafim2021@mail.ru>
2022-03-14 19:18:11 +0300

    init youtybe clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+import styled from "styled-components";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Header />
+      <AppContainer className="border border-info">
+        <Sidebar />
+        <BootstrapContainer fluid classsName="border border-warning">
+          <HomeScreen />
+        </BootstrapContainer>
+      </AppContainer>
+    </>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  display: flex;
+  height: 90vh;
+`;
+
+const BootstrapContainer = styled(Container)``;
---
+++ src/components/Header.tsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaBars } from "react-icons/fa";
+import { MdApps, MdNotifications } from "react-icons/md";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <HeaderContainer className="border border-dark">
+      <HeaderMenu size={26} />
+      <HeaderLogo
+        src="https://pngimg.com/uploads/youtube/youtube_PNG2.png"
+        alt="logo"
+      />
+
+      <form>
+        <input type="text" placeholder="Search" />
+        <button type="submit">
+          <AiOutlineSearch size={22} />
+        </button>
+      </form>
+
+      <HeaderIcons>
+        <MdNotifications size={28} />
+        <MdApps size={28} />
+        <img
+          src="https://tr-static.eodev.com/files/d1e/96e1725f89132ee2a1113a8db2a7f107.jpg"
+          alt="avatar"
+        />
+      </HeaderIcons>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 3rem;
+  background-color: var(--black-primary);
+  height: 10vh;
+  width: 100%;
+
+  @media (max-width: 520px) {
+    padding: 1rem;
+
+    form {
+      flex: 1;
+    }
+  }
+
+  > form {
+    flex: 0.6;
+    display: flex;
+    padding: 0.1rem;
+    margin: 0 1rem;
+    border-radius: 3px;
+    border: 1.2px solid;
+    background-color: var(--black-secondary);
+
+    > input {
+      width: 100%;
+      border: none;
+      font-weight: 500;
+      background: transparent;
+      padding: 0.3rem;
+      color: var(--text-color);
+
+      &:focus {
+        outline: none;
+      }
+    }
+    > button {
+      padding: 0 1.25rem;
+      color: var(--text-color);
+      background: transparent;
+      border: none;
+      &:focus {
+        border: none;
+      }
+    }
+  }
+`;
+
+const HeaderMenu = styled(FaBars)`
+  display: none;
+
+  @media (max-width: 520px) {
+    display: block;
+  }
+`;
+
+const HeaderLogo = styled.img`
+  width: 30px;
+  height: 30px;
+  object-fit: contain;
+  display: block;
+
+  @media (max-width: 520px) {
+    display: none;
+  }
+`;
+
+const HeaderIcons = styled.div`
+  flex: 0.15;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  > img {
+    border-radius: 50%;
+    width: 40px;
+    object-fit: contain;
+    margin-left: 5px;
+  }
+
+  @media (max-width: 520px) {
+    > *:not(img) {
+      display: none;
+    }
+  }
+`;
