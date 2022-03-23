0c3c4cf6e13ec6c8b6be62304e3f30e24f0acaa3
Serafim Sen <sima.serafim2@mail.ru>
2022-03-23 18:29:35 +0300

    readi for deployment

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: "AIzaSyAU-_FBkmp1yRR1Nn7ES5qnqU6dGopRiVo",
+    key: process.env.REACT_APP_API_KEY,
   },
 });
 
